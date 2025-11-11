2f14ac655fbdf4acc79a480db89543f9fc02be22
thanhtruc14ct-art <thanhtruc14ct@gmail.com>
2025-11-11 16:53:10 +0700

    update files

---
+++ qgis2web_2025_11_08-20_12_03_680253/data/UBNDTP_11.js
@@ -0,0 +1,1 @@
+var json_UBNDTP_11 = {"type":"FeatureCollection","name":"UBNDTP_11","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"1"},"geometry":{"type":"Point","coordinates":[105.788325019191035,10.037230637881088]}}]}
